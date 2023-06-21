332d4a431791828d2de946cb9526b865191fc651
IDCDEV03 <idcdev03@iddrivers.co.th>
2023-06-21 11:01:00 +0700

    update

---
+++ resources/views/layouts/simple/css.blade.php
@@ -0,0 +1,14 @@
+
+ <!-- ======= All CSS Plugins here ======== -->
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/css/plugins/swiper-bundle.min.css')}}">
+ <link rel="stylesheet" type="text/css" href="{{asset('assets/css/plugins/glightbox.min.css')}}">
+ 
+<!-- Custom Style CSS -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+  <!-- Plugin css -->
+<link rel="stylesheet" type="text/css" href="{{asset('assets/css/vendor/bootstrap.min.css')}}">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500&display=swap" rel="stylesheet">
+
---
+++ resources/views/layouts/simple/script.blade.php
@@ -0,0 +1,9 @@
+  <!-- All Script JS Plugins here  -->
+
+<script src="{{asset('assets/js/vendor/popper.js')}}"></script>
+<script src="{{asset('assets/js/vendor/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/plugins/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('assets/js/plugins/glightbox.min.js')}}"></script>
+<!-- Customscript js -->
+<script src="{{asset('assets/js/script.js')}}"></script>
+
